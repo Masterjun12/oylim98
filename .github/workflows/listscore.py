2afc5e195568c3f71f37541bbfc3e9695a54e206
oylim98 <82173315+oylim98@users.noreply.github.com>
2021-05-29 09:33:31 +0900

    Update listscore.py

--- .github/workflows/listscore.py
+++ .github/workflows/listscore.py
@@ -6,17 +6,28 @@ sci = []
 tot = [] 
 ave = []
 
-for i in range(3): n = input('이름대시오') k = int(input('국어성적')) e = int(input('영어성적')) m = int(input('수학성적')) s = int(input('과학성적'))
+for i in range(3): 
+  n = input('이름대시오') 
+  k = int(input('국어성적')) 
+  e = int(input('영어성적')) 
+  m = int(input('수학성적')) 
+  s = int(input('과학성적'))
 
 name.append(n)
 kor.append(k)
 eng.append(e)
 mat.append(m)
 sci.append(s)
-for x in range(3): totlist = kor[x] + eng[x] + mat[x] + sci[x] avelist = totlist/4
+for x in range(3): 
+  totlist = kor[x] + eng[x] + mat[x] + sci[x] 
+  avelist = totlist/4
 
 tot.append(totlist)
 ave.append(avelist)
-for y in range(3): print('\n학생이름: ',name[y]) print('\n합산점수:',tot[y]) print('\n평균: ',ave[y])
+for y in range(3): 
+  print('\n학생이름: ',name[y]) 
+  print('\n합산점수:',tot[y]) 
+  print('\n평균: ',ave[y])
 
-all = sum(tot)/3 print('\n\n전체:',all)
+all = sum(tot)/3 
+print('\n\n전체:',all)
